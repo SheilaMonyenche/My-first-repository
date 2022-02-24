08c80e9b27bb2dac52281dbe156b4851a6d18fab
SheilaMonyenche <97979878+SheilaMonyenche@users.noreply.github.com>
2022-02-24 19:59:28 +0300

    Factorial file

---
+++ factorial.py
@@ -0,0 +1,14 @@
+#factoria.py
+#program to compute the factorial of a number
+#illustrates for loop with an accumulator
+
+def main ():
+    n=int(input("please enter a whole number:"))
+    fact=1
+    for factor in range (n,1 ,-1):
+        fact= fact * factor
+
+        print("The factorial of", n, "is", fact)
+
+main()        
+    
